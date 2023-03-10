2da88055bd33d17d2186d9bdba001a8751794ca0
Igor-Shipunov <Igor-Shipunov@users.noreply.github.com>
2023-03-10 20:07:07 +0300

    Initial commit

---
+++ src/main/java/Antimen/ReactShawa/ReactShawaApplication.java
@@ -0,0 +1,13 @@
+package Antimen.ReactShawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactShawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactShawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Antimen/ReactShawa/ReactShawaApplicationTests.java
@@ -0,0 +1,13 @@
+package Antimen.ReactShawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactShawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
